98a85a718bebbaa86dc601c4bc6c3b2ca431e720
lakshitagit <lakshitasethi40@gmail.com>
2025-11-26 00:22:35 +0530

    implement dock presentation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { Navbar,Welcome } from "#components";
+import { Dock, Navbar,Welcome } from "#components";
 
 const App = () => {
   return (
     <main>
       <Navbar></Navbar>
       <Welcome></Welcome>
+      <Dock></Dock>
     </main>
   );
 };
